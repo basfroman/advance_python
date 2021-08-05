7b9424f446f68f4468a46539cd1fb54929ad278e
basfroman <basfroman@gmail.com>
2021-08-05 02:48:12 -0700

    Edit memoization module

--- memoization.py
+++ memoization.py
@@ -1,13 +1,13 @@
 import time
 
 
-def fib(x):
+def fib_recursion(x):
     """Calculate fibonacci using regular recursion."""
     if x == 0:
         return 0
     if x == 1:
         return 1
-    return fib(x-2) + fib(x-1)
+    return fib_recursion(x-2) + fib_recursion(x-1)
 
 
 def fib_memo(x: int, cache: list = None):
@@ -33,12 +33,12 @@ def fib_memo(x: int, cache: list = None):
 
 if __name__ == '__main__':
 
-    for f in [fib, fib_memo]:
+    for f in [fib_memo, fib_recursion]:
 
         start = time.time()
-        xx = 4
+        xx = 37
         fibonacci = f(xx)
 
         stop = round(time.time()-start, 4)  # seconds with round 4 numbers after float
 
-        print('function:', f.__name__, '| time:', stop, '| result: ', fibonacci)
+        print(f'Function: {f.__name__}({xx}) | Execution time is {stop} seconds. | Result is {fibonacci}.')
